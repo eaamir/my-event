4cab0135b758a4b4ced1eff5c067964305a8ba28
mahbodmo <mahbod.mortazavi@yahoo.com>
2025-10-04 11:10:33 +0330

    access granted for localhost:8080

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:8080'], // your frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   // Swagger config
   const config = new DocumentBuilder()
     .setTitle('Auth API')
